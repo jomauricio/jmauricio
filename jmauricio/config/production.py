0494e19ba2f443467cff2e83c6c5a8625603d117
Mauricio <grandemau@gmail.com>
2015-02-25 23:20:08 -0300

    Alterando cinfigurações do ambiente de Produção

--- jmauricio/config/production.py
+++ jmauricio/config/production.py
@@ -58,44 +58,44 @@ class Production(Common):
 
     # STORAGE CONFIGURATION
     # See: http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += (
-        'storages',
-    )
+    #INSTALLED_APPS += (
+    #    'storages',
+    #)
 
     # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    #STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
     # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-    AWS_ACCESS_KEY_ID = values.SecretValue()
-    AWS_SECRET_ACCESS_KEY = values.SecretValue()
-    AWS_STORAGE_BUCKET_NAME = values.SecretValue()
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
+    #AWS_ACCESS_KEY_ID = values.SecretValue()
+    #AWS_SECRET_ACCESS_KEY = values.SecretValue()
+    #AWS_STORAGE_BUCKET_NAME = values.SecretValue()
+    #AWS_AUTO_CREATE_BUCKET = True
+    #AWS_QUERYSTRING_AUTH = False
 
     # see: https://github.com/antonagestam/collectfast
-    AWS_PRELOAD_METADATA = True
-    INSTALLED_APPS += ('collectfast', )
+    #AWS_PRELOAD_METADATA = True
+    #INSTALLED_APPS += ('collectfast', )
 
     # AWS cache settings, don't change unless you know what you're doing:
-    AWS_EXPIRY = 60 * 60 * 24 * 7
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (
-            AWS_EXPIRY, AWS_EXPIRY)
-    }
+    #AWS_EXPIRY = 60 * 60 * 24 * 7
+    #AWS_HEADERS = {
+    #    'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (
+    #        AWS_EXPIRY, AWS_EXPIRY)
+    #}
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-    STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+    #STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
     # END STORAGE CONFIGURATION
 
     # EMAIL
-    DEFAULT_FROM_EMAIL = values.Value('jmauricio <noreply@jmauricio.net.br>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[jmauricio] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
-    SERVER_EMAIL = EMAIL_HOST_USER
+    #DEFAULT_FROM_EMAIL = values.Value('jmauricio <noreply@jmauricio.net.br>')
+    #EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    #EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    #EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    #EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    #EMAIL_SUBJECT_PREFIX = values.Value('[jmauricio] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    #EMAIL_USE_TLS = True
+    #SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
 
     # TEMPLATE CONFIGURATION
@@ -111,17 +111,17 @@ class Production(Common):
     # CACHING
     # Only do this here because thanks to django-pylibmc-sasl and pylibmc
     # memcacheify is painful to install on windows.
-    try:
+    # try:
         # See: https://github.com/rdegges/django-heroku-memcacheify
-        from memcacheify import memcacheify
-        CACHES = memcacheify()
-    except ImportError:
-        CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
+    #    from memcacheify import memcacheify
+    #    CACHES = memcacheify()
+    # except ImportError:
+    #    CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
     # END CACHING
 
     # DATABASE CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
-    DATABASES = values.DatabaseURLValue('postgres://ncaiumntjttqjd:y8DO3Kvrh1CNlaNOotUEks254W@ec2-107-21-104-188.compute-1.amazonaws.com:5432/d1bikkt2kt2bvu')
+    # DATABASES = values.DatabaseURLValue('postgres://ncaiumntjttqjd:y8DO3Kvrh1CNlaNOotUEks254W@ec2-107-21-104-188.compute-1.amazonaws.com:5432/d1bikkt2kt2bvu')
     # END DATABASE CONFIGURATION
 
     # Your production stuff: Below this line define 3rd party library settings
