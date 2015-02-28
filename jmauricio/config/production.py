a34b8496147da3235150d95cf6966192bfd3a735
Mauricio <grandemau@gmail.com>
2015-02-28 00:24:36 -0300

    Alteração das configurações de produção

--- jmauricio/config/production.py
+++ jmauricio/config/production.py
@@ -10,12 +10,12 @@ Production Configurations
 from configurations import values
 
 # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-try:
-    from S3 import CallingFormat
-    AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
-except ImportError:
+#try:
+#    from S3 import CallingFormat
+#    AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+#except ImportError:
     # TODO: Fix this where even if in Dev this class is called.
-    pass
+#   pass
 
 from .common import Common
 
@@ -88,14 +88,14 @@ class Production(Common):
     # END STORAGE CONFIGURATION
 
     # EMAIL
-    #DEFAULT_FROM_EMAIL = values.Value('jmauricio <noreply@jmauricio.net.br>')
-    #EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    #EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    #EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    #EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    #EMAIL_SUBJECT_PREFIX = values.Value('[jmauricio] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    #EMAIL_USE_TLS = True
-    #SERVER_EMAIL = EMAIL_HOST_USER
+    DEFAULT_FROM_EMAIL = values.Value('jmauricio <noreply@jmauricio.net.br>')
+    EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    EMAIL_SUBJECT_PREFIX = values.Value('[jmauricio] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    EMAIL_USE_TLS = True
+    SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
 
     # TEMPLATE CONFIGURATION
